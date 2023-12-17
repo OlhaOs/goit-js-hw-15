3ddd296a4e8ba306029ab50ff41dbafed5ba64dd
OlhaOs <olalaadka@gmail.com>
2023-12-17 11:27:15 +0200

    remove prev tasks

--- js/task-05.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector('#name-input');
-const inputValue = document.querySelector('#name-output');
-
-inputEl.addEventListener('input', handleInput);
-
-function handleInput(e) {
-  inputValue.textContent = e.currentTarget.value.trim() || 'Anonymous';
-}
