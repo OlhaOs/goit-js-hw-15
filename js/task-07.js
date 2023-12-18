836c85fabbc441ba2026716c1f36cd7600d3cb7e
OlhaOs <olalaadka@gmail.com>
2023-12-18 10:54:21 +0200

    set fs for span

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEL = document.querySelector('#text');
 
+spanEL.style.fontSize = `${inputEl.value}px`;
+
 inputEl.addEventListener('input', handleInputRange);
 
 function handleInputRange() {
